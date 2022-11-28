ab6c693469ab4ec527b5c5e763ad18236c4152c1
ldh <574427343@qq.com>
2022-11-28 10:07:19 +0800

    clear rep

--- MusicSite/Framework.Admin/Contracts/IBannerContract.cs
+++
@@ -1,9 +0,0 @@
-﻿using Framework.Models.Entities;
-using Framework.Utility.ViewModels; 
-namespace Framework.Admin.Contracts
-{
-    public interface IBannerContract
-    {
-        TData<List<Biz_Banner>> GetAllBanner();
-    }
-}
--- MusicSite/NUnitTest/Usings.cs
+++
@@ -1,3 +0,0 @@
-global using NUnit.Framework;
-global using Framework.EF.Context;
-global using Framework.Repository;
